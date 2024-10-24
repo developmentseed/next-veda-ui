95800b00fd13480990dada35054e2e207bd3d3a9
Hanbyul Jo <hanbyul@nyu.edu>
2024-10-24 13:06:30 -0400

    Use data selector modal

--- app/(datasets)/exploration/exploration.tsx
+++ app/(datasets)/exploration/exploration.tsx
@@ -1,7 +1,10 @@
 'use client';
 import React, { useState } from 'react';
 import { useAtom } from 'jotai';
-import { ExplorationAndAnalysis, timelineDatasetsAtom } from 'app/lib';
+import Link from 'next/link';
+import { getDatasets } from 'app/blog/utils/mdx';
+import { ExplorationAndAnalysis, timelineDatasetsAtom, DatasetSelectorModal } from 'app/lib';
+
 
 export default function ExplorationAnalysis({
   datasets
@@ -10,11 +13,45 @@ export default function ExplorationAnalysis({
 }) {
   const [explorationDatasets, setExplorationDatasets] = useAtom(timelineDatasetsAtom);
   const [datasetModalRevealed, setDatasetModalRevealed] = useState(
-    !datasets.length
+    !!datasets.length
   );
-  const openModal = () => setDatasetModalRevealed(true);
+  const openModal = () => {setDatasetModalRevealed(true);}
+  const closeModal = () => {setDatasetModalRevealed(false);}
+  const transformData = () => {
+    const data = datasets?.map((post) => ({
+      ...post.metadata
+    }));
+
+    const result = data?.map((d) => {
+      const updatedTax = d.taxonomy.map((t) => {
+        const updatedVals = t.values.map((v) => {
+          return {
+            id: v.replace(/ /g, '_').toLowerCase(),
+            name: v
+          }
+        })
+        return {...t, values: updatedVals}
+      })
+      return {...d, taxonomy: updatedTax}
+    })
+
+    return result
+  };
+
+  const transformed = transformData();
 
   return (
-    <ExplorationAndAnalysis datasets={explorationDatasets} setDatasets={setExplorationDatasets} openDatasetsSelectionModal={openModal} />
+    <div>
+    <DatasetSelectorModal
+    revealed={datasetModalRevealed}
+    close={closeModal}
+    linkProperties={{
+      LinkElement: Link,
+      pathAttributeKeyName: 'href'
+    }}
+    datasetPathName={'data-catalog'}
+    datasets={transformed}
+    />
+  </div>
   )
 };
