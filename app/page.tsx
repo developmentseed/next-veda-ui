b069d0ec06cef3aae319f8609caa1a2622f7ff68
Sandra Hoang <sandrahoang686@gmail.com>
2024-12-03 11:48:57 -0500

    add pagehero to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import { PageHero } from '@lib';
 
-export default function Page() {
+export default function HomePage() {
   return (
-    <section>
-      <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
-        Next.js VEDA Template Instance
-      </h1>
-
-      <p className='mb-4'>
-        This is a test Next JS instance to figure out how to use VEDA specific
-        MDX files with NEXT JS Instance.
-      </p>
-    </section>
+    <PageHero
+      title="VEDA UI Template Instance"
+      description="This is this template instance using the veda-ui refactored npm library and in integration with USWDS"
+      coverSrc="./public/images/root-welcome--cover.jpg"
+      coverAlt="welcome-image"
+    />
   );
 }
