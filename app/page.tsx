20fc109fdc8e89802029a5f0349492d588ad672b
Sandra Hoang <sandrahoang686@gmail.com>
2024-12-03 11:55:10 -0500

    lint doublequote error fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { PageHero } from '@lib';
 export default function HomePage() {
   return (
     <PageHero
-      title="VEDA UI Template Instance"
-      description="This is this template instance using the veda-ui refactored npm library and in integration with USWDS"
-      coverSrc="./public/images/root-welcome--cover.jpg"
-      coverAlt="welcome-image"
+      title='VEDA UI Template Instance'
+      description='This is this template instance using the veda-ui refactored npm library and in integration with USWDS'
+      coverSrc='./public/images/root-welcome--cover.jpg'
+      coverAlt='welcome-image'
     />
   );
 }
