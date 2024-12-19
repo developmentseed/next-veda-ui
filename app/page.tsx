69b83523b99cb635372a6edb9ecdaa9203777938
Anthony Boyd <92742765+aboydnw@users.noreply.github.com>
2024-12-19 12:33:37 -0800

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -68,77 +68,76 @@ export default function Page() {
       </p>
     </section>
   );
+
+
+  function Audience() {
+    return (
+      <Fold>
+        <FoldHeader>
+          <FoldTitle>VEDA serves a wide scientific audience</FoldTitle>
+          <FoldLead>
+            VEDA makes science based on NASA datasets inclusive, accessible, and
+            reproducible.
+          </FoldLead>
+        </FoldHeader>
+        <FoldBody>
+          <AudienceList>
+            <li>
+              <BlockAudience>
+                <BlockAudienceProse>
+                  <h3>Earth scientists</h3>
+                  <p>
+                    Easily access high quality Earth data from NASA and its
+                    associated partners.
+                  </p>
+                </BlockAudienceProse>
+                <BlockAudienceMedia>
+                  <img
+                    src={imgScientists}
+                    alt='United States at night seen from orbit'
+                  />
+                </BlockAudienceMedia>
+              </BlockAudience>
+            </li>
+
+            <li>
+              <BlockAudience>
+                <BlockAudienceProse>
+                  <h3>Academic researchers</h3>
+                  <p>
+                    Support your research efforts with an open source
+                    cloud-computing platform backed by NASA data.
+                  </p>
+                </BlockAudienceProse>
+                <BlockAudienceMedia>
+                  <img
+                    src={imgResearcher}
+                    alt='Philippine woman scientist working at a desk in front of a computer'
+                  />
+                </BlockAudienceMedia>
+              </BlockAudience>
+            </li>
+
+            <li>
+              <BlockAudience>
+                <BlockAudienceProse>
+                  <h3>Science enthusiasts</h3>
+                  <p>
+                    Discover the latest in Earth Observation, how that might impact environmental change,
+                    and how this might impact where you live.
+                  </p>
+                </BlockAudienceProse>
+                <BlockAudienceMedia>
+                  <img
+                    src={imgEnthusiasts}
+                    alt={`An intricate maze of small lakes and waterways define the Yukon Delta at the confluence of Alaska's Yukon and Kuskokwim Rivers with the frigid Bering Sea.`}
+                  />
+                </BlockAudienceMedia>
+              </BlockAudience>
+            </li>
+          </AudienceList>
+        </FoldBody>
+      </Fold>
+    );
+  }
 }
-
-function Audience() {
-  return (
-    <Fold>
-      <FoldHeader>
-        <FoldTitle>VEDA serves a wide scientific audience</FoldTitle>
-        <FoldLead>
-          VEDA makes science based on NASA datasets inclusive, accessible, and
-          reproducible.
-        </FoldLead>
-      </FoldHeader>
-      <FoldBody>
-        <AudienceList>
-          <li>
-            <BlockAudience>
-              <BlockAudienceProse>
-                <h3>Earth scientists</h3>
-                <p>
-                  Easily access high quality Earth data from NASA and its
-                  associated partners.
-                </p>
-              </BlockAudienceProse>
-              <BlockAudienceMedia>
-                <img
-                  src={imgScientists}
-                  alt='United States at night seen from orbit'
-                />
-              </BlockAudienceMedia>
-            </BlockAudience>
-          </li>
-
-          <li>
-            <BlockAudience>
-              <BlockAudienceProse>
-                <h3>Academic researchers</h3>
-                <p>
-                  Support your research efforts with an open source
-                  cloud-computing platform backed by NASA data.
-                </p>
-              </BlockAudienceProse>
-              <BlockAudienceMedia>
-                <img
-                  src={imgResearcher}
-                  alt='Philippine woman scientist working at a desk in front of a computer'
-                />
-              </BlockAudienceMedia>
-            </BlockAudience>
-          </li>
-
-          <li>
-            <BlockAudience>
-              <BlockAudienceProse>
-                <h3>Science enthusiasts</h3>
-                <p>
-                  Discover the latest in Earth Observation, how that might impact environmental change,
-                  and how this might impact where you live.
-                </p>
-              </BlockAudienceProse>
-              <BlockAudienceMedia>
-                <img
-                  src={imgEnthusiasts}
-                  alt={`An intricate maze of small lakes and waterways define the Yukon Delta at the confluence of Alaska's Yukon and Kuskokwim Rivers with the frigid Bering Sea.`}
-                />
-              </BlockAudienceMedia>
-            </BlockAudience>
-          </li>
-        </AudienceList>
-      </FoldBody>
-    </Fold>
-  );
-}
-
-export default Audience;
